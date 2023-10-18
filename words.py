ba4758adba8030ce197e970321110fd98c6c5288
Vladislav Balabaev <vv.balabaev@gmail.com>
2023-10-18 13:20:57 +0300

    parse words list

---
+++ words.py
@@ -0,0 +1,27 @@
+import pickle
+import pandas as pd
+from pathlib import Path
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+path = Path('data')
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+
+if __name__ == '__main__':
+    driver = webdriver.Chrome(options=chrome_options)
+
+    driver.get('https://web.archive.org/web/20230205160246/https://studynow.ru/dicta/allwords')
+
+    words_table = driver.find_element(By.CSS_SELECTOR, f'#wordlist').get_attribute('outerHTML')
+    words_table = pd.concat(pd.read_html(words_table))
+
+    words = list(words_table[1])
+
+    driver.close()
+
+    with open(path / 'all_words.pkl', 'wb') as f:
+        pickle.dump(words, f)
